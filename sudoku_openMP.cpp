a0f71c771f452c0c6881bd7365dae712171a5e4d
Bella Liu <renata2001bella@gmail.com>
2023-05-03 22:49:57 -0400

    updated openmp

--- sudoku_openMP.cpp
+++ sudoku_openMP.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
-#include <omp.h>
+#include <string.h>
+#include <chrono>
+#include "omp.h"
 using namespace std;
 
 #include "sudoku_helper.cpp"
 
 // Number 0 in input board indicates an empty cell
 #define EMPTY 0
+
+double start;
 
 bool findEmptyCell(int N, int** grid, int& row, int& col)
 {
@@ -46,7 +50,7 @@ bool isSafe(int N, int** grid, int row,
 	return true;
 }
 
-bool SolveSudoku(int N, int** grid)
+int SolveSudoku(int N, int** grid, int level)
 {
 	int row, col;
 	if (!findEmptyCell(N, grid, row, col))
@@ -55,16 +59,45 @@ bool SolveSudoku(int N, int** grid)
 	for (int num = 1; num <= N; num++)
 	{
 		if (isSafe(N, grid, row, col, num))
+		#pragma omp task firstprivate(grid, row, col, num, level) shared(start) final(level>1)
 		{
-			grid[row][col] = num;
-			if (SolveSudoku(N, grid))
-				return true;
+			int** copy_grid;	
+			copy_grid = new int*[N];
+			for (int i = 0; i < N; i++)
+				copy_grid[i] = new int[N];		
 
-			grid[row][col] = EMPTY;
+			for (int x=0 ; x < N; x++){
+  				for (int y=0 ; y < N; y++){
+					copy_grid[x][y] = grid[x][y];
+				}
+			}	
+						
+			// memcpy(copy_grid,grid,N * N * sizeof(int));		
+
+			copy_grid[row][col] = num;  			
+			// grid[row][col] = num;
+
+			if (SolveSudoku(N, copy_grid, level+1)){
+				// return true;
+				cout << '\n' << "Solution board: " << '\n';
+				printGrid(N,copy_grid);
+                double end = omp_get_wtime();
+                double time_spent = end - start;  				
+				cout << "Computation Time: " << time_spent << '\n';
+				exit(0);
+
+			}
+
+			for (int i = 0; i < N; i++)
+				delete copy_grid[i];	
+			free(copy_grid);
+
+			// grid[row][col] = EMPTY;
 		}
 	}
 	//backtracks
-	return false;
+    #pragma omp taskwait
+	return 0;
 }
 
 int main(int argc, char* argv[])
@@ -88,20 +121,12 @@ int main(int argc, char* argv[])
 	cout << "Input board:" << '\n';
 	printGrid(N, grid);
 
-	auto start = chrono::system_clock::now();
-	bool result = SolveSudoku(N, grid);
-	auto end = chrono::system_clock::now();
+	start = omp_get_wtime();
 
-	chrono::duration<double> elapsed_seconds = end - start;
-
-	// print result
-	if (result){
-		cout << '\n' << "Solution board: " << '\n';
-		printGrid(N, grid);
-		cout << "Computation Time: " << elapsed_seconds.count() << '\n';
-	}
-	else{
-		cout << "No solution exists";
+	#pragma omp parallel shared(grid) num_threads(16)
+	#pragma omp single nowait	
+	{
+		SolveSudoku(N, grid, 1);
 	}
 
 	return 0;
